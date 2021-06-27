79b832d83af6a197ea8b6ad76db352b14ab56400
Dylan <wdylan907@gmail.com>
2021-06-27 18:50:26 -0400

    edit app.py

--- app.py
+++ app.py
@@ -14,17 +14,14 @@ key = {
     'phi': 'Philadelphia'
 }
 
-def query(city, year):
-    query = dict(city=key[city], year=int(year))
-    return dumps(db.find(query))
-
 @app.route("/")
 def index():    
     return render_template("index.html")
 
 @app.route('/<city>/<year>')
 def route(city, year):
-    return query(city, year)
+    query = dict(city=key[city], year=int(year))
+    return dumps(db.find(query))
 
 if __name__ == "__main__":
     app.run(debug=True,threaded=True)
