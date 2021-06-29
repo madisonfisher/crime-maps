b3d7323d9d8ff926ac31fa587604689ccfc7ed45
Dylan <wdylan907@gmail.com>
2021-06-28 21:03:40 -0400

    edit app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/<city>/<year>')
 def route(city, year):
-    collection = 'buf'+f'{year}'
+    collection = f'{city}+{year}'
     return dumps(db[collection].find())
 
 if __name__ == "__main__":
