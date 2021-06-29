e23e239b39ca2a8abe18ea560fa274663a5a2ba9
Dylan <wdylan907@gmail.com>
2021-06-28 23:55:15 -0400

    add map

--- static/script.js
+++ static/script.js
@@ -1,15 +1,14 @@
-function init() {
-  populateDropdowns();
-  setTimeout(updateData, 100);
-}
-
 function updateData() {
   document.getElementById("button").disabled = true;
   document.getElementById("button").textContent = "Loading data...";
   city = document.getElementById("selectCity").value;
   year = document.getElementById("selectYear").value;
   d3.json(`/${city}/${year}`)
-    .then((data) => console.log(data))
+    .then((data) => {
+      //plot stuff here
+      updateMap(data);
+      console.log(data);
+    })
     .catch((error) => console.log(error))
     .finally(() => {
       document.getElementById("button").disabled = false;
@@ -44,5 +43,26 @@ function populateDropdowns() {
   }
 }
 
+function updateMap(data) {
+  let map = L.map("main-map").setView([33.76, -84.43], 10);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: API_KEY,
+    }
+  ).addTo(map);
+  // data.forEach((entry) => {
+  //   L.marker([entry.lat, entry.long]).addTo(map);
+  // });
+  L.marker([data[0].lat, data[0].long]).addTo(map).bindPopup(data[0].crime);
+}
+
 d3.selectAll("#button").on("click", updateData);
-init();
+populateDropdowns();
+setTimeout(updateData, 100);
